345370152abb69e48146513d7613c958bcd120ad
viktorie94 <hoangminhlong94@gmail.com>
2022-03-21 14:05:42 +0100

    Validation_email

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
-
+import {  FormsModule ,ReactiveFormsModule } from '@angular/forms';
 import { LoginPage } from './login.page';
 
 @NgModule({
@@ -13,6 +12,7 @@ import { LoginPage } from './login.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     LoginPageRoutingModule
   ],
   declarations: [LoginPage]
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  ionicForm: FormGroup;
+  isSubmitted = false;
+  
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+     // name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+      
+    //  password: ['', [Validators.required, Validators.pattern('^[0-9]+$')]]
+    })
   }
-
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      console.log(this.ionicForm.value)
+    }
+  }
 }
